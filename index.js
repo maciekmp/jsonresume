0260255411bac0e1f6fccb40e964b84bf33c5f7d
Maciej Płusa <maciekmp@gmail.com>
2023-11-06 18:50:15 +0100

    Add new theme BLANK to repository

---
+++ index.js
@@ -0,0 +1,4 @@
+var theme = require('./jsonresume-theme-blank/index.js');
+module.exports = {
+  render: theme.render,
+};
---
+++ jsonresume-theme-blank/README.md
@@ -0,0 +1,3 @@
+# JSON Resume Blank template
+
+Template displays data from template without presonal information.
